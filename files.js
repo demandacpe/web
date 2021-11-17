390f70c44a036076520f937a7953ac1bb7fc98bb
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-11-17 10:33:33 -0300

    feat: add new files

--- files.js
+++ files.js
@@ -11,63 +11,116 @@ function fromDrive(link) {
 
 var files = [
   {
-    type: "audio",
-    url: fromDrive(
-      "https://drive.google.com/file/d/12G0iue9d1KNJB0zl0WYT28pr2J2Wr8ma/view?usp=sharing"
-    ),
-    name: "Audio 1",
-    id: "1",
-  },
-  {
-    type: "audio",
-    url: fromDrive(
-      "https://drive.google.com/file/d/1gO8UeUrXbVo8LT-woLWyawReQ-DVJPu2/view?usp=sharing"
-    ),
-    name: "Audio 2",
-    id: "2",
-  },
-  {
-    type: "audio",
-    url: fromDrive(
-      "https://drive.google.com/file/d/1SfWP50WLmFmznE98hmj3ESNK0yYIMvGq/view?usp=sharing"
-    ),
-    name: "Audio 3",
-    id: "3",
-  },
-  {
-    type: "pdf",
-    url: fromDrive(
-      "https://drive.google.com/file/d/1wzZrZkjCmPNgtnNIXamiXvNS9mXHFJao/view?usp=sharing"
-    ),
-    name: "Pdf(em branco)",
-    id: "4",
-  },
-  {
     type: "vid",
     url: fromDrive(
       "https://drive.google.com/file/d/1DeKj4Xy6MJw0XMt8Hykd_-zV7loz8zzH/view?usp=sharing"
     ),
-    name: "Video 1",
-    id: "5",
+    name: "Video 01",
+    id: "1",
   },
+
   {
-    type: "img",
-    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs.yimg.com%2Fny%2Fapi%2Fres%2F1.2%2FrFRofxQihQMPS99PmOF0.g--%2FYXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYxMy4wNDM0NzgyNjA4Njk2%2Fhttps%3A%2F%2Fmedia-mbst-pub-ue1.s3.amazonaws.com%2Fcreatr-uploaded-images%2F2020-06%2Fe0e0a050-b722-11ea-ab75-8a3069ba59e1&f=1&nofb=1",
-    name: "Imagem",
-    id: "6",
+    type: "audio",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1FoT6vWlthZ5En4VVJTCefAesS8HvgsvJ/view?usp=sharing"
+    ),
+    name: "2016, tortura com agressões",
+    id: "8",
   },
   {
     type: "audio",
     url: fromDrive(
-      "https://drive.google.com/file/d/1SfWP50WLmFmznE98hmj3ESNK0yYIMvGq/view?usp=sharing"
+      "https://drive.google.com/file/d/12G0iue9d1KNJB0zl0WYT28pr2J2Wr8ma/view?usp=sharing"
     ),
-    name: "Audio",
-    id: "7",
+    name: "Áudio 0.52",
+    id: "9",
   },
   {
-    type: "img",
-    url: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs.yimg.com%2Fny%2Fapi%2Fres%2F1.2%2FrFRofxQihQMPS99PmOF0.g--%2FYXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTYxMy4wNDM0NzgyNjA4Njk2%2Fhttps%3A%2F%2Fmedia-mbst-pub-ue1.s3.amazonaws.com%2Fcreatr-uploaded-images%2F2020-06%2Fe0e0a050-b722-11ea-ab75-8a3069ba59e1&f=1&nofb=1",
-    name: "Imagem",
-    id: "8",
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1gO8UeUrXbVo8LT-woLWyawReQ-DVJPu2/view?usp=sharing"
+    ),
+    name: "Áudio 1.36",
+    id: "10",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1SfWP50WLmFmznE98hmj3ESNK0yYIMvGq/view?usp=sharing"
+    ),
+    name: "Áudio 1.37",
+    id: "11",
+  },
+  {
+    type: "audio",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1knvYOlBZDkIChWaeEnSrh2Z2mkEj8oZF/view?usp=sharing"
+    ),
+    name: "Áudio 1.49",
+    id: "12",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1XTgH1uuvfL8ZO8y60uR7yhCsJ6K2fmFo/view?usp=sharing"
+    ),
+    name: "Áudio 3.47",
+    id: "13",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1_QOt9FyHEXXI_j8AGAdoHzIOjw-3KbxR/view?usp=sharing"
+    ),
+    name: "Áudio 4.29",
+    id: "14",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1a_IvF43a0neTxdW-6fgxrLSJjsmH6SI_/view?usp=sharing"
+    ),
+    name: "Áudio 5.07",
+    id: "15",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1iIecXdjVY02QTYMKOB6m-ZYyI47Vo2kI/view?usp=sharing"
+    ),
+    name: "Áudio 7.03",
+    id: "16",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1NpBWqtIza8GuX5QvGBlotKNQ_krSVKvk/view?usp=sharing"
+    ),
+    name: "Áudio 10.29",
+    id: "17",
+  },
+  {
+    type: "vid",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1C_pjTJHCJ7XUdMmgivjRlWsP_NE4hhuo/view?usp=sharing"
+    ),
+    name: "Áudio 11.02",
+    id: "18",
+  },
+  {
+    type: "audio",
+    url: fromDrive(
+      "https://drive.google.com/file/d/12dqg_uAiVb5ApKSRfWuQwItgipe2HVF9/view?usp=sharing"
+    ),
+    name: "Áudio grande amplificado",
+    id: "19",
+  },
+  {
+    type: "audio",
+    url: fromDrive(
+      "https://drive.google.com/file/d/1fMwdY-tYmZQo3-2CznrIEYC9gQThxZ1Z/view?usp=sharing"
+    ),
+    name: "Confissão, ameaças, agressões, rejeição filha, apropriação indébita, coerção e manipulação mental",
+    id: "20",
   },
 ];
